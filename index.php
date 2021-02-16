423878917041a6bc804485b71e2040306c0709ec
Darkksinh <78621169+Darkksinh@users.noreply.github.com>
2021-02-16 04:25:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,97 +1,141 @@
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+ <head> 
+  <meta charset="utf-8"> 
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> 
+  <meta name="description" content=""> 
+  <meta name="author" content=""> 
+  <link href="https://indrijunanda.github.io/RuangAdmin/img/logo.png" rel="icon"> 
+  <title>REST API - FDCI CYBER ART</title> 
+  <link href="https://indrijunanda.github.io/RuangAdmin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> 
+  <link href="https://indrijunanda.github.io/RuangAdmin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"> 
+  <link href="https://indrijunanda.github.io/RuangAdmin/css/ruang-admin.min.css" rel="stylesheet"> 
+  <meta property="og:title" content="REST API TOOLS & DOWNLOADER ONLINE "> 
+  <meta property="og:description" content="Isi aja biar ga di block fb wkakkaak"> 
+  <meta property="og:image" content="https://www.cloudways.com/blog/wp-content/uploads/Getting-Started-With-Magento-2-Rest-API-Banner.jpg"> 
+  <meta property="og:url" content="index.html&quot;"> 
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/androidstudio.min.css"> 
+  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"></script> 
+  <script>hljs.initHighlightingOnLoad();</script> 
+ </head> 
+ <body id="page-top"> 
+  <div id="wrapper"> <!-- Sidebar --> 
+   <ul class="navbar-nav sidebar sidebar-light accordion bg-info" id="accordionSidebar" style="color:#fff;"> <a class="sidebar-brand d-flex align-items-center justify-content-center bg-info" style="color:#fff;" href="index.php"> 
+     <div class="sidebar-brand-icon"> 
+      <img src="https://fdci.se/wp-content/uploads/2020/08/1596067888-picsay-1-picsay-1.png"> 
+     </div> </a> 
+    <hr class="sidebar-divider my-0"> 
+    <li class="nav-item active"> <a class="nav-link" href="index.php"> <i class="fas fa-fw fa-tachometer-alt"></i> <span>Dashboard</span></a> </li> 
+    <hr class="sidebar-divider"> 
+    <div class="sidebar-heading">
+      Features 
+    </div> 
+    <li class="nav-item"> <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap" aria-expanded="true" aria-controls="collapseBootstrap"> <i class="fas fa-fw fa-arrow-down"></i> <span>Sosmed API</span> </a> 
+     <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar"> 
+      <div class="bg-white py-2 collapse-inner rounded"> 
+       <h6 class="collapse-header">Sosmed API</h6> <a class="collapse-item" href="./api/fb.php">Facebook Get Video Link</a> <a class="collapse-item" href="./api/insta.php">Instagram Get Video/ Photo Link</a> <a class="collapse-item" href="./api/pinboard.php">Pinterest Board Lookup</a> <a class="collapse-item" href="./api/yt.php">Youtube Get Video Link</a> <a class="collapse-item" href="./api/twitter.php">Twitter </a> <a class="collapse-item" href="./api/pin.php">Search Image Pinterest</a> 
+      </div> 
+     </div> </li> 
+    <li class="nav-item"> <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm" aria-expanded="true" aria-controls="collapseForm"> <i class="fas fa-fw fa-book"></i> <span>Educational API</span> </a> 
+     <div id="collapseForm" class="collapse" aria-labelledby="headingForm" data-parent="#accordionSidebar"> 
+      <div class="bg-white py-2 collapse-inner rounded"> 
+       <h6 class="collapse-header">Educational Features</h6> <a class="collapse-item" href="form_basics.php">Wikipedia API</a> <a class="collapse-item" href="form_advanceds.php">Brainly API</a> <a class="collapse-item" href="form_advanceds.php">Quora API</a> 
+      </div> 
+     </div> </li> 
+    <li class="nav-item"> <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable" aria-expanded="true" aria-controls="collapseTable"> <i class="fas fa-fw fa-table"></i> <span>Tables</span> </a> 
+     <div id="collapseTable" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar"> 
+      <div class="bg-white py-2 collapse-inner rounded"> 
+       <h6 class="collapse-header">Tables</h6> <a class="collapse-item" href="simple-tables.php">Simple Tables</a> <a class="collapse-item" href="datatables.php">DataTables</a> 
+      </div> 
+     </div> </li> 
+    <li class="nav-item"> <a class="nav-link" href="ui-colors.php"> <i class="fas fa-fw fa-palette"></i> <span>UI Colors</span> </a> </li> 
+    <hr class="sidebar-divider"> 
+    <div class="sidebar-heading">
+      Examples 
+    </div> 
+    <li class="nav-item"> <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePage" aria-expanded="true" aria-controls="collapsePage"> <i class="fas fa-fw fa-columns"></i> <span>Pages</span> </a> 
+     <div id="collapsePage" class="collapse" aria-labelledby="headingPage" data-parent="#accordionSidebar"> 
+      <div class="bg-white py-2 collapse-inner rounded"> 
+       <h6 class="collapse-header">Example Pages</h6> <a class="collapse-item" href="login.php">Login</a> <a class="collapse-item" href="register.php">Register</a> <a class="collapse-item" href="404.php">404 Page</a> <a class="collapse-item" href="blank.php">Blank Page</a> 
+      </div> 
+     </div> </li> 
+    <li class="nav-item"> <a class="nav-link" href="charts.php"> <i class="fas fa-fw fa-chart-area"></i> <span>Charts</span> </a> </li> 
+    <hr class="sidebar-divider"> 
+    <div class="version" id="version-ruangadmin">
+     Version 1.1
+    </div> 
+   </ul> <!-- Sidebar --> 
+   <div id="content-wrapper" class="d-flex flex-column"> 
+    <div id="content"> <!-- TopBar --> 
+     <nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top bg-info"> <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3"> <i class="fa fa-bars"></i> </button> 
+      <ul class="navbar-nav ml-auto"> 
+       <div class="topbar-divider d-none d-sm-block"></div> 
+       <li class="nav-item dropdown no-arrow"> <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <img class="img-profile rounded-circle" src="https://www.freepngimg.com/thumb/android/67303-hacker-hackers-simulator-safe-logo-hacking-android.png" style="max-width: 60px"> <span class="ml-2 d-none d-lg-inline text-white small">Maman Ketoprak</span> </a> 
+        <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown"> <a class="dropdown-item" href="#"> <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i> Profile </a> <a class="dropdown-item" href="#"> <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i> Settings </a> <a class="dropdown-item" href="#"> <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i> Activity Log </a> 
+         <div class="dropdown-divider"></div> <a class="dropdown-item" href="javascript:void(0);" data-toggle="modal" data-target="#logoutModal"> <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i> Logout </a> 
+        </div> </li> 
+      </ul> 
+     </nav> <!-- Topbar --> <!-- Container Fluid--> <!-- Container Fluid--> 
+     <div class="container-fluid" id="container-wrapper"> <!-- Attention --> 
+      <div id="demo" class="alert alert-info alert-dismissible" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span id="demo" aria-hidden="true">×</span> </button> 
+       <script>
+var d = new Date();
+var n = d.getHours();
+var menit = d.getMinutes();
+var detik= d.getSeconds();
+document.write(n +":"+ menit +":"+ detik); </script>4:23:3 Hi There welcome :) 
+      </div> <!-- Attention --> 
+      <div class="row"> 
+       <div class="col-lg-6"> 
+        <div class="card mb-4"> 
+         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between"> 
+          <h6 class="m-0 font-weight-bold text-primary">Feature Available</h6> 
+         </div> 
+         <div class="card-body"> 
+          <li> Facebook </li> 
+         </div> 
+         <div class="card-footer">
+           Card Footer 
+         </div> 
+        </div> 
+       </div> 
+      </div> <!--Row--> <!-- Modal Logout --> 
+      <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout" aria-hidden="true"> 
+       <div class="modal-dialog" role="document"> 
+        <div class="modal-content"> 
+         <div class="modal-header"> 
+          <h5 class="modal-title" id="exampleModalLabelLogout">Ohh No!</h5> <button type="button" class="close" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">×</span> </button> 
+         </div> 
+         <div class="modal-body"> 
+          <p>Are you sure you want to logout?</p> 
+         </div> 
+         <div class="modal-footer"> <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Cancel</button> <a href="login.html" class="btn btn-primary">Logout</a> 
+         </div> 
+        </div> 
+       </div> 
+      </div> 
+     </div> <!--Container Fluid--> 
+    </div> <!-- Footer --> 
+    <footer class="sticky-footer bg-white"> 
+     <div class="container my-auto"> 
+      <div class="copyright text-center my-auto"> <span>copyright © <script> 
 
-	<head>
-		<title>Português: Simples Assim</title>
-		<meta charset="utf-8">
-		<link rel="stylesheet" type="text/css" href="reset.css">
-		<link rel="stylesheet" type="text/css" href="estilos.css">
-		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Crimson+Text:400,400italic,600">
-	</head>
+document.write(new Date().getFullYear()); 
+</script>2021 - developed by <b><a href="https://api.fdci.se/" target="_blank">Abdul Muttaqin</a></b> </span> 
+      </div> 
+     </div> 
+    </footer> <!-- Footer --> 
+   </div> 
+  </div> <!-- Scroll to top --> <a class="scroll-to-top rounded" href="#page-top"> <i class="fas fa-angle-up"></i> </a> 
+  <script src="https://indrijunanda.github.io/RuangAdmin/vendor/jquery/jquery.min.js"></script> 
+  <script src="https://indrijunanda.github.io/RuangAdmin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> 
+  <script src="https://indrijunanda.github.io/RuangAdmin/vendor/jquery-easing/jquery.easing.min.js"></script> 
+  <script src="https://indrijunanda.github.io/RuangAdmin/js/ruang-admin.min.js"></script> 
+  <script> 
+	
+</body>
 
-	<body>
-
-	<header>
-		<h1>Português: Simples Assim<h1>
-	</header>
-
-	<main>
-
-		<p id="introducao" class="introducao">"Português: Simples Assim" é uma brincadeira de proposta de reformulação e simplificação da língua portuguesa que visaria a simplificação do uso do alfabeto e seus respectivos equivalentes sonoros. Essa ideia surgiu como uma brincadeira a partir de discussões e reflexições entre amigos sobre a complexidade e falta de lógica prática do uso das letras e seus respectivos sons.</p>
-
-		<p class="introducao">É importante salientar que a proposta não visa discutir a origem das palavras, tampouco discutir o fatores históricos ou acadêmicos. O intuíto é brincar e refletir sobre a simplificação, praticidade e acessíbilidade que tais mudanças poderiam gerar. Mas uma ideia para simplicidade e praticidade da utilização do alfabeto de forma que exista uma lógica e coerência entre a fonética e a escrita de forma que todos possam saber como escrever sem a necessidade de um treino e pré-conhecimento das palavras, como ocorre atualmente.</p>
-
-		<p class="introducao">Para a leitura das propostas é altamente recomendo que mantenham a mente aberta que o texto se trata de uma brincadeira. Qualquer incoerência ou equívoco pode ser reportado pelas redes sociais linkadas no menu à direita.</p>
-
-		<ul>
-
-			<li id="letrac">
-				<h3 class="letra letra-impar">C</h3>
-				<p class="explicacao">Pense nos diferentes sons que a letra C reproduz ao concatenar-se as vogais: ca, ce, ci, co, cu, cão. Percebe-se claramente a diferença sonora entre ca, co, cu, cão e ce, ci. Enquanto, nos principais casos, a letra soa parecida com Q ou K, nos outros dois soa como S. A ideia de simplificação se basea em definir o som que a letra deve fazer e aplicá-la em todos os casos, sem exceções. Dessa forma, palavras que precisem soar como S usaram o "S" enquanto o C se responsabiliza pelo som de Q/K, como em "ca". Alguns exemplos de palavras que seriam reescritas usando C: caixa, ceijo (queijo), escina (esquina), âncora, cutelo, cão.</p>
-				<p class="explicacao">Alguns defendem o uso na letra K para os sons realizados por C, sendo assim o C excluído de nosso alfabeto. Pessoalmente eu prefiro o uso do C e exclusão do K, jás que a letra C nos é natural, enquanto o K nos sõa estrangeito e, por muito tempo, não fez parte do alfabeto portuguê</p>
-				<p class="explicacao">As alterações no uso da letra C impactam diretamente no uso das letras K e Q, as quais são tratadas em suas respectiva sessões.</p>
-			</li>
-			<li id="letrag">
-				<h3 class="letra letra-par">G</h3>
-				<p class="explicacao">Semelhante a letra C, a letra G realiza demasiadas funções sonoras, basta pensar em suas combinações com as vogais: ga, ge, gi, go, gu e gão. E ainda temos a combinação de g+u+e e g+u+i que mudam a sonoridade e as deixam parecidas com o ga, go, gu, gão. A simplificação aqui atuaria de tal forma: o G passar a exercer o som igual em "ga", mesmo para "ge" e "gi", de forma que a combinação g+u+e e g+u+i não é mais necessária, pois "ge" passa a ter o som de "gue" e "gi" passar a ter o som de "gui". Já os sons de "ge" e "gi" já são bem representados pela combinação dessas mesmas vogais com a letra J.</p>
-				<p class="explicacao">Alguns exemplos de palavras que seriam reescritas usando G: gato, gerreiro (guerreiro), gizo (guizo), goleiro, angústia, pagão.</p>
-			</li>
-			<li id="letrah">
-				<h3 class="letra letra-impar">H</h3>
-				<p class="explicacao">A letra H é uma letra que não faz som por si só, e auxília outras letras a fazerem sons que já podem ser realizados por letras existentes em nosso alfabetp. Uma curiosidade é que nem a escrita de H (agá) possui a letra H.</p>
-
-			</li>
-			<li id="letraj">
-				<h3 class="letra letra-par">J</h3>
-				<p class="explicacao">Cras dignissim odio sed ornare suscipit. Aliquam viverra elit nec porttitor tincidunt. Pellentesque scelerisque, massa vel tempor aliquet, purus arcu aliquam nunc, eu hendrerit risus lacus et mauris. Sed convallis urna lacus, in tempus odio ultricies at. Proin quam ligula, lobortis nec consectetur et, maximus ac arcu. Etiam semper lacus enim, in mollis ipsum faucibus eu. Maecenas posuere sit amet nisi vitae ultrices. Phasellus pulvinar finibus tincidunt. Curabitur euismod augue risus, sit amet condimentum eros bibendum vel. Morbi ultrices lobortis elit ac hendrerit. Praesent dapibus blandit luctus.</p>
-			</li>
-			<li id="letraq">
-				<h3 class="letra letra-impar">Q</h3>
-				<p class="explicacao">Cras dignissim odio sed ornare suscipit. Aliquam viverra elit nec porttitor tincidunt. Pellentesque scelerisque, massa vel tempor aliquet, purus arcu aliquam nunc, eu hendrerit risus lacus et mauris. Sed convallis urna lacus, in tempus odio ultricies at. Proin quam ligula, lobortis nec consectetur et, maximus ac arcu. Etiam semper lacus enim, in mollis ipsum faucibus eu. Maecenas posuere sit amet nisi vitae ultrices. Phasellus pulvinar finibus tincidunt. Curabitur euismod augue risus, sit amet condimentum eros bibendum vel. Morbi ultrices lobortis elit ac hendrerit. Praesent dapibus blandit luctus.</p>
-			</li>
-			<li id="letras">
-				<h3 class="letra letra-par">S</h3>
-				<p class="explicacao">Cras dignissim odio sed ornare suscipit. Aliquam viverra elit nec porttitor tincidunt. Pellentesque scelerisque, massa vel tempor aliquet, purus arcu aliquam nunc, eu hendrerit risus lacus et mauris. Sed convallis urna lacus, in tempus odio ultricies at. Proin quam ligula, lobortis nec consectetur et, maximus ac arcu. Etiam semper lacus enim, in mollis ipsum faucibus eu. Maecenas posuere sit amet nisi vitae ultrices. Phasellus pulvinar finibus tincidunt. Curabitur euismod augue risus, sit amet condimentum eros bibendum vel. Morbi ultrices lobortis elit ac hendrerit. Praesent dapibus blandit luctus.</p>
-			</li>
-			<li id="letrax">
-				<h3 class="letra letra-impar">X</h3>
-				<p class="explicacao">Cras dignissim odio sed ornare suscipit. Aliquam viverra elit nec porttitor tincidunt. Pellentesque scelerisque, massa vel tempor aliquet, purus arcu aliquam nunc, eu hendrerit risus lacus et mauris. Sed convallis urna lacus, in tempus odio ultricies at. Proin quam ligula, lobortis nec consectetur et, maximus ac arcu. Etiam semper lacus enim, in mollis ipsum faucibus eu. Maecenas posuere sit amet nisi vitae ultrices. Phasellus pulvinar finibus tincidunt. Curabitur euismod augue risus, sit amet condimentum eros bibendum vel. Morbi ultrices lobortis elit ac hendrerit. Praesent dapibus blandit luctus.</p>
-			</li>
-			<li id="letraz">
-				<h3 class="letra letra-par">Z</h3>
-				<p class="explicacao">Cras dignissim odio sed ornare suscipit. Aliquam viverra elit nec porttitor tincidunt. Pellentesque scelerisque, massa vel tempor aliquet, purus arcu aliquam nunc, eu hendrerit risus lacus et mauris. Sed convallis urna lacus, in tempus odio ultricies at. Proin quam ligula, lobortis nec consectetur et, maximus ac arcu. Etiam semper lacus enim, in mollis ipsum faucibus eu. Maecenas posuere sit amet nisi vitae ultrices. Phasellus pulvinar finibus tincidunt. Curabitur euismod augue risus, sit amet condimentum eros bibendum vel. Morbi ultrices lobortis elit ac hendrerit. Praesent dapibus blandit luctus.</p>
-			</li>
-			<li id="letraskwy">
-				<h3 class="letra letra-impar">K, W e Y</h3>
-				<p class="explicacao">Cras dignissim odio sed ornare suscipit. Aliquam viverra elit nec porttitor tincidunt. Pellentesque scelerisque, massa vel tempor aliquet, purus arcu aliquam nunc, eu hendrerit risus lacus et mauris. Sed convallis urna lacus, in tempus odio ultricies at. Proin quam ligula, lobortis nec consectetur et, maximus ac arcu. Etiam semper lacus enim, in mollis ipsum faucibus eu. Maecenas posuere sit amet nisi vitae ultrices. Phasellus pulvinar finibus tincidunt. Curabitur euismod augue risus, sit amet condimentum eros bibendum vel. Morbi ultrices lobortis elit ac hendrerit. Praesent dapibus blandit luctus.</p>
-			</li>
-		</ul>
-	</main>
-
-	<footer>
-		&copy;<a href="https://github.com/ricardosander/portuges-simples-asim">Ricardo Sander 2016</a>
-	</footer>
-
-	<aside>
-		<nav>
-			<ul>
-				<li><a href="#introducao">Introdução</a></li>
-				<li><a href="#letrac">C</a></li>
-				<li><a href="#letrag">G</a></li>
-				<li><a href="#letrah">H</a></li>
-				<li><a href="#letraj">J</a></li>
-				<li><a href="#letraq">Q</a></li>
-				<li><a href="#letras">S</a></li>
-				<li><a href="#letrax">X</a></li>
-				<li><a href="#letraz">Z</a></li>
-				<li><a href="#letraskwy">K, W e Y</a></li>
-			</ul>
-		</nav>
-		<ul class="icones-sociais">
-			<li><a class="github" 	href="https://github.com/ricardosander">GitHub</a></li>
-			<li><a class="twitter" 	href="https://twitter.com/_ricardosander">Twitter</a></li>
-			<li><a class="linkedin" href="https://www.linkedin.com/in/ricardo-sander-lopes-572a9b65">LinkedIn</a></li>
-		</ul>
-	</aside>
-	</body>
+</html></script>
+ </body>
 </html>
