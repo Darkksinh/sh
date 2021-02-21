f549e8c91b0910cc98558fe948c33e75cebb60d6
Darkksinh <78621169+Darkksinh@users.noreply.github.com>
2021-02-21 00:19:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,249 +1,114 @@
-<html><head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link href="https://indrijunanda.github.io/RuangAdmin/img/logo.png" rel="icon">
-  <title>REST API - FDCI CYBER ART</title>
-  <link href="https://indrijunanda.github.io/RuangAdmin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://indrijunanda.github.io/RuangAdmin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="https://indrijunanda.github.io/RuangAdmin/css/ruang-admin.min.css" rel="stylesheet">
-  <meta property="og:title" content="REST API TOOLS &amp; DOWNLOADER ONLINE ">
-<meta property="og:description" content="Isi aja biar ga di block fb wkakkaak">
-<meta property="og:image" content="https://www.cloudways.com/blog/wp-content/uploads/Getting-Started-With-Magento-2-Rest-API-Banner.jpg">
-<meta property="og:url" content="index.html&quot;">
-
-  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/styles/androidstudio.min.css">
-<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.9.0/highlight.min.js"></script>
-<script>hljs.initHighlightingOnLoad();</script>
-  
-</head>
-
-<body id="page-top">
-  <div id="wrapper">
-    <!-- Sidebar -->
-    <ul class="navbar-nav sidebar sidebar-light accordion bg-info" id="accordionSidebar" style="color:#fff;">
-      <a class="sidebar-brand d-flex align-items-center justify-content-center bg-info" style="color:#fff;" href="index.php">
-        <div class="sidebar-brand-icon">
-          <img src="https://fdci.se/wp-content/uploads/2020/08/1596067888-picsay-1-picsay-1.png">
-        </div>
-  
-      </a>
-      <hr class="sidebar-divider my-0">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
-      </li>
-      <hr class="sidebar-divider">
-      <div class="sidebar-heading">
-        Features
-      </div>
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap" aria-expanded="true" aria-controls="collapseBootstrap">
-          <i class="fas fa-fw fa-arrow-down"></i>
-          <span>Sosmed API</span>
-        </a>
-        <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Sosmed API</h6>
-            <a class="collapse-item" href="./api/fb.php">Facebook  Get Video Link</a>
-            <a class="collapse-item" href="./api/insta.php">Instagram Get Video/ Photo Link</a>
-            <a class="collapse-item" href="./api/pinboard.php">Pinterest Board Lookup</a>
-            <a class="collapse-item" href="./api/yt.php">Youtube Get Video Link</a>
-            <a class="collapse-item" href="./api/twitter.php">Twitter </a>
-            <a class="collapse-item" href="./api/pin.php">Search Image Pinterest</a>
-          </div>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm" aria-expanded="true" aria-controls="collapseForm">
-          <i class="fas fa-fw fa-book"></i>
-          <span>Educational API</span>
-        </a>
-        <div id="collapseForm" class="collapse" aria-labelledby="headingForm" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Educational Features</h6>
-            <a class="collapse-item" href="form_basics.php">Wikipedia API</a>
-            <a class="collapse-item" href="form_advanceds.php">Brainly API</a>
-            <a class="collapse-item" href="form_advanceds.php">Quora API</a>
-          </div>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTable" aria-expanded="true" aria-controls="collapseTable">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span>
-        </a>
-        <div id="collapseTable" class="collapse" aria-labelledby="headingTable" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Tables</h6>
-            <a class="collapse-item" href="simple-tables.php">Simple Tables</a>
-            <a class="collapse-item" href="datatables.php">DataTables</a>
-          </div>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="ui-colors.php">
-          <i class="fas fa-fw fa-palette"></i>
-          <span>UI Colors</span>
-        </a>
-      </li>
-      <hr class="sidebar-divider">
-      <div class="sidebar-heading">
-        Examples
-      </div>
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePage" aria-expanded="true" aria-controls="collapsePage">
-          <i class="fas fa-fw fa-columns"></i>
-          <span>Pages</span>
-        </a>
-        <div id="collapsePage" class="collapse" aria-labelledby="headingPage" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Example Pages</h6>
-            <a class="collapse-item" href="login.php">Login</a>
-            <a class="collapse-item" href="register.php">Register</a>
-            <a class="collapse-item" href="404.php">404 Page</a>
-            <a class="collapse-item" href="blank.php">Blank Page</a>
-          </div>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="charts.php">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span>
-        </a>
-      </li>
-      <hr class="sidebar-divider">
-      <div class="version" id="version-ruangadmin">Version 1.1</div>
-    </ul>
-    <!-- Sidebar -->
-    <div id="content-wrapper" class="d-flex flex-column">
-      <div id="content">
-        <!-- TopBar -->
-        <nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top bg-info">
-          <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-          </button>
-          <ul class="navbar-nav ml-auto">
-            
-            <div class="topbar-divider d-none d-sm-block"></div>
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img class="img-profile rounded-circle" src="https://www.freepngimg.com/thumb/android/67303-hacker-hackers-simulator-safe-logo-hacking-android.png" style="max-width: 60px">
-                <span class="ml-2 d-none d-lg-inline text-white small">Maman Ketoprak</span>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Profile
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Settings
-                </a>
-                <a class="dropdown-item" href="#">
-                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Activity Log
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="javascript:void(0);" data-toggle="modal" data-target="#logoutModal">
-                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                  Logout
-                </a>
-              </div>
-            </li>
-          </ul>
-        </nav>        <!-- Topbar -->
-
-        <!-- Container Fluid-->
-        <!-- Container Fluid-->
-        <div class="container-fluid" id="container-wrapper">
-          <!-- Attention -->
-          <div id="demo" class="alert alert-info alert-dismissible" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-              <span id="demo" aria-hidden="true">×</span>
-            </button>
-        <script>
-var d = new Date();
-var n = d.getHours();
-var menit = d.getMinutes();
-var detik= d.getSeconds();
-document.write(n +":"+ menit +":"+ detik); </script>5:15:37   Hi There welcome :) 
-          </div>
-          <!-- Attention -->
-          
-
-          <div class="row">
-            <div class="col-lg-6">
-              <div class="card mb-4">
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">Feature Available</h6>
-                </div>
-                <div class="card-body">
-                  <li> Facebook </li>
-                </div>
-                <div class="card-footer">
-                  Card Footer
-                </div>
-              </div>
-            </div>
-          </div>
-          <!--Row-->
-
-          <!-- Modal Logout -->
-          <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabelLogout" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabelLogout">Ohh No!</h5>
-                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                  </button>
-                </div>
-                <div class="modal-body">
-                  <p>Are you sure you want to logout?</p>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Cancel</button>
-                  <a href="login.html" class="btn btn-primary">Logout</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-        </div>
-        <!---Container Fluid-->
-      </div>
-      <!-- Footer -->
-      <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>copyright © <script> 
-
-document.write(new Date().getFullYear()); 
-</script>2021 - developed by
-              <b><a href="https://api.fdci.se/" target="_blank">Abdul Muttaqin</a></b>
-            </span>
-          </div>
-        </div>
-      </footer>
-      <!-- Footer -->
-    </div>
-  </div>
-
-  <!-- Scroll to top -->
-  <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-  </a>
-
-  <script src="https://indrijunanda.github.io/RuangAdmin/vendor/jquery/jquery.min.js"></script>
-  <script src="https://indrijunanda.github.io/RuangAdmin/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="https://indrijunanda.github.io/RuangAdmin/vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="https://indrijunanda.github.io/RuangAdmin/js/ruang-admin.min.js"></script>
-<script> 
-	
-</body>
-
-</html></script></body></html>
+[
+    {
+	"teks": "CPF GERADO: 884.506.490-56",
+	"image": "https://i.ibb.co/tMh2xTv/20210220-164937.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 974.796.180-69",
+	"image": "https://i.ibb.co/YtdRHwG/20210220-165118.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 187.616.300-39",
+	"image": "https://i.ibb.co/9gm51cN/20210220-165211.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 429.449.510-33",
+	"image": "https://i.ibb.co/cksjh66/20210220-165321.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 315.970.360-64",
+    "image": "https://i.ibb.co/r2kTTMv/20210220-165415.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 170.609.870-70",
+    "image": "https://i.ibb.co/mhSxdQD/20210220-165627.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 171.618.810-56",
+    "image": "https://i.ibb.co/w6rWVtR/20210220-165744.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 647.518.770-46",
+    "image": "https://i.ibb.co/4Fj0mkD/20210220-165904.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 278.383.240-25",
+    "image": "https://i.ibb.co/SBrr0LZ/20210220-165951.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 400.138.290-33",
+    "image": "https://i.ibb.co/rtsC3H2/20210220-170059.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 791.164.510-82",
+    "image": "https://i.ibb.co/82tsDwS/20210220-170153.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 483.059.070-07",
+    "image": "https://i.ibb.co/zHPDGmt/20210220-170238.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 707.888.020-03",
+    "image": "https://i.ibb.co/mT8g6yN/20210220-170319.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 239.006.510-43",
+    "image": "https://i.ibb.co/rQqFV2f/20210220-170436.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 884.506.490-56",
+	"image": "https://i.ibb.co/tMh2xTv/20210220-164937.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 974.796.180-69",
+	"image": "https://i.ibb.co/YtdRHwG/20210220-165118.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 187.616.300-39",
+	"image": "https://i.ibb.co/9gm51cN/20210220-165211.jpg"
+    },
+    {
+	"teks": "CPF GERADO: 429.449.510-33",
+	"image": "https://i.ibb.co/cksjh66/20210220-165321.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 315.970.360-64",
+    "image": "https://i.ibb.co/r2kTTMv/20210220-165415.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 170.609.870-70",
+    "image": "https://i.ibb.co/mhSxdQD/20210220-165627.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 171.618.810-56",
+    "image": "https://i.ibb.co/w6rWVtR/20210220-165744.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 647.518.770-46",
+    "image": "https://i.ibb.co/4Fj0mkD/20210220-165904.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 278.383.240-25",
+    "image": "https://i.ibb.co/SBrr0LZ/20210220-165951.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 400.138.290-33",
+    "image": "https://i.ibb.co/rtsC3H2/20210220-170059.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 791.164.510-82",
+    "image": "https://i.ibb.co/82tsDwS/20210220-170153.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 483.059.070-07",
+    "image": "https://i.ibb.co/zHPDGmt/20210220-170238.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 707.888.020-03",
+    "image": "https://i.ibb.co/mT8g6yN/20210220-170319.jpg"
+    },
+    {
+    "teks": "CPF GERADO: 239.006.510-43",
+    "image": "https://i.ibb.co/rQqFV2f/20210220-170436.jpg"
+    }
+]
